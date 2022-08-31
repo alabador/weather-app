8691a190c1effe7ac4e3f98314aba2a2e3c39d57
alabador <austinlabador@gmail.com>
2022-08-31 15:29:45 -0700

    Create mobile html layout of weather app, implement tailwind and dependencies

--- main.js
+++
@@ -1,9 +0,0 @@
-
-
-async function getWeather() {
-    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=fb47c3a686e3aefa6c40797334a495df');
-    const data = await response.json();
-    console.log(data);
-}
-
-getWeather();
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+        massive: '6rem',
+      },
+    },
+  },
+  plugins: [],
+}
