0e58814d58e7bacc28d3a507d3f2c0f12f785d5a
alabador <austinlabador@gmail.com>
2022-09-01 15:49:09 -0700

    Create object literal with weather functions

--- src/main.js
+++ src/main.js
@@ -2,6 +2,19 @@ const form = document.querySelector('#search-form');
 
 form.addEventListener("submit", saveSearchValue);
 
+let weather = {
+    apiKey: "fb47c3a686e3aefa6c40797334a495df",
+    getWeather: function(city) {
+        fetch("http://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&APPID="
+            + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((random) => console.log(random));
+    },
+
+}
 
 function saveSearchValue(e) {
     e.preventDefault();
@@ -9,23 +22,13 @@ function saveSearchValue(e) {
     return searchbar.value;
 }
 
-async function getWeather() {
-    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Seattle&APPID=fb47c3a686e3aefa6c40797334a495df');
-    const data = await response.json();
-    // console.log(data);
-}
-
-getWeather();
-
 /*Used to get location name and etc information*/
 async function getLocation() {
     const response = await fetch('http://api.openweathermap.org/geo/1.0/direct?q=Guangzhou&appid=fb47c3a686e3aefa6c40797334a495df');
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
 }
 
 //Functions will check if country is inside US. If inside US, will use
 //name of city and state.
 //If not in US, will use name of city and Country. 
-
-getLocation();
