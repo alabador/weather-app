cd9efcff4c505d5fb7a6db73f236d102748bb421
alabador <austinlabador@gmail.com>
2023-02-13 00:33:05 -0800

    Add data themes and fix search bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.js",
     "./index.html"
   ],
   theme: {
     screens: {
-      // 'sm': '640px',
-      // // => @media (min-width: 640px) { ... }
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
 
-      // 'md': '768px',
-      // // => @media (min-width: 768px) { ... }
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
 
-      // 'lg': '1024px',
-      // // => @media (min-width: 1024px) { ... }
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
 
-      // 'xl': '1280px',
-      // // => @media (min-width: 1280px) { ... }
-
-      // '2xl': '1536px',
-      // // => @media (min-width: 1536px) { ... }
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
     },
     extend: {
       fontSize: {
@@ -28,4 +26,7 @@ module.exports = {
     },
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: ["coffee", "luxury", "pastel", "business", "retro", "garden", "forest", "night"],
+  },
 }
